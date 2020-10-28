2d5f8e6c146722229c04ead6acf0841a9ad2bf3f
Chris Barrow <chris@archive.studio>
2020-10-28 12:58:08 +0000

    Added preview check to plugin

--- plugin.js
+++ plugin.js
@@ -1,12 +1,19 @@
-export default function(context, inject) {
-  const options = JSON.parse('<%= JSON.stringify(options) %>')
-  const { url, namespace } = options
+export default function (context, inject) {
+  const options = JSON.parse('<%= JSON.stringify(options) %>');
+  const { url, namespace } = options;
+
+  const { query, enablePreview } = context;
+
+  if (query['x-craft-live-preview']) {
+    const token = query.token || '';
+    enablePreview({ token });
+  }
 
   inject(namespace, {
     get: (slug) => {
-      const { $http, $preview } = context
-      const token = $preview ? `?token=${$preview.token}` : ''
-      return $http.$get(`${url}${slug}.json${token}`)
-    }
-  })
+      const { $http, $preview } = context;
+      const token = $preview ? `?token=${$preview.token}` : '';
+      return $http.$get(`${url}${slug}.json${token}`);
+    },
+  });
 }
